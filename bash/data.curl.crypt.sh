becfa82ea2996726cb408ee0fe965de64bc613d3
Danilo Garkut <garbini@gmail.com>
2019-09-27 16:49:22 -0300

    Sample tidy

--- bash/data.curl.crypt.sh
+++ bash/data.curl.crypt.sh
@@ -2,7 +2,7 @@
 
 #######################################
 
-crypt_this=$(Pad "{\"a\":\"Hello\", \"login\":\"John\", \"password\":\"John12345\"}" "PKCS#5")
+crypt_this=$(Pad "{\"a\":\"Hello\", \"login\":\"Cloud\", \"password\":\"Aeris\"}" "PKCS#5")
 key=$(Pad "SecretKey" 0 0 32)
 iv=$(Pad "0A0908FC" 0 0 32)
 url="https://reach.this_url.com"
