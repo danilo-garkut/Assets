08a2e688f2183ed60054c5cad2c6db50235ba5d5
Danilo G. Veraszto <garbini@gmail.com>
2019-11-15 17:26:43 -0300

    Keeping compatibility with classManager

--- js/general.js
+++ js/general.js
@@ -235,3 +235,52 @@ function restartAnin(el, className, callback, callback_max_control)
 	}
 	return el;
 }
+
+
+function classManager(el, classes_names_array, action)
+{	
+	if(el === undefined)
+		return;
+	var classes_names = el.className;
+	if (el.tagName.toLowerCase() === "svg")
+	{
+		classes_names = el.className.baseVal;
+	}
+	var classes = classes_names.split(" ");
+	if (action === "add" || action === undefined)
+	{
+		var has_added = false;
+		for (var w = 0 ; w < classes_names_array.length ; w++)
+		{
+			if (classes.indexOf(classes_names_array[w]) === -1)
+			{
+				classes.push(classes_names_array[w]);
+				has_added = true;
+			}
+		}
+		var classes_joined = classes.join(" ");
+		if (el.tagName.toLowerCase() === "svg")
+		{
+			el.className.baseVal = classes_joined;
+			return has_added;
+		}
+		el.className = classes_joined;
+		return has_added;
+	}
+	for (var w = 0 ; w < classes_names_array.length ; w++)
+	{
+		var index = classes.indexOf(classes_names_array[w]);		
+		if (index !== -1)
+		{
+			classes.splice(index, 1);
+		}
+	}
+	if (el.tagName.toLowerCase() === "svg")
+	{
+		el.className.baseVal = classes.join(" ");
+		return;
+	}
+	el.className = classes.join(" ");
+}
+
+
